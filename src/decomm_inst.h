9263e7f12721a82cf189e0b93ee9b5ab4eef7960
Katy Huff <katyhuff@gmail.com>
2014-06-27 13:16:43 -0700

    edits...

--- src/decomm_inst.h
+++ src/decomm_inst.h
@@ -9,18 +9,20 @@ namespace decomm {
 
 /// @class DecommInst
 ///
-/// This Institution is intended as a skeleton to guide the implementation of
-/// new Institution agents.
-///
+/// This Institution is specifically designed for market-driven decommissioning
+/// of reactors. It utlizes manager mixin classes to assist in this task. It was
+/// created for the specific purpose of runing the fco code-to-code comparison.
+/// However, it was designed to be sufficiently generic for general use.
+/// 
 /// The DecommInst class inherits from the Institution class and is dynamically
 /// loaded by the Agent class when requested.
 ///
 /// @section intro Introduction
-/// Place an introduction to the agent here.
+/// The DecommInst decommissions facilities of a particular type if and when a
+/// material criteria occurs in the markets.
 ///
 /// @section agentparams Agent Parameters
-/// Place a description of the required input parameters which define the
-/// agent implementation.
+/// The facilities that are governed by this institution, the material availability 
 ///
 /// @section optionalparams Optional Parameters
 /// Place a description of the optional input parameters to define the
@@ -60,3 +62,57 @@ class DecommInst : public cyclus::Institution {
 }  // namespace decomm
 
 #endif  // CYCLUS_DECOMMS_DECOMM_INST_H_
+
+
+
+
+// Deployinst.h
+#ifndef _DEPLOYINST_H
+#define _DEPLOYINST_H
+
+#include "cyclus.h"
+
+#include <utility>
+#include <set>
+#include <map>
+
+namespace cycamore {
+
+typedef std::map<int, std::vector<std::string> > BuildSched;
+
+///  @class DeployInst
+///  The DeployInst class inherits from the Institution
+///  class and is dynamically loaded by the Agent class when requested.
+///
+///  This agent implements a simple institution agent that deploys
+///  specific facilities as defined explicitly in the input file.
+class DeployInst : public cyclus::Institution {
+ public:
+  DeployInst(cyclus::Context* ctx);
+
+  virtual ~DeployInst();
+
+  #pragma cyclus decl clone
+
+  #pragma cyclus decl schema
+
+  #pragma cyclus decl infiletodb
+
+  #pragma cyclus decl initfromdb
+
+  #pragma cyclus decl initfromcopy
+
+  #pragma cyclus decl snapshot
+
+  #pragma cyclus def annotations
+
+  #pragma cyclus note {"doc": "An institution that owns, operates, and " \
+                              "deploys facilities manually defined in " \
+                              "the input file."}
+
+  virtual void Build(cyclus::Agent* parent);
+
+ protected:
+  /// a collection of orders to build
+  BuildSched build_sched_;
+};
